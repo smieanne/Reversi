dc258b7d9f03f15ba94952588304549527cd2fe3
smieanne <mieanne.sugiyama@g-seed.co.jp>
2024-08-22 11:17:20 +0900

    次の石を判断する処理

--- src/domain/model/turn/turn.ts
+++ src/domain/model/turn/turn.ts
@@ -8,7 +8,7 @@ export class Turn {
   constructor(
     private _gameId: number,
     private _turnCount: number,
-    private _nextDisc: Disc,
+    private _nextDisc: Disc | undefined,
     private _move: Move | undefined,
     private _board: Board,
     private _endAt: Date
@@ -27,8 +27,7 @@ export class Turn {
 
     const nextBoard = this._board.place(move)
 
-    // TODO 次の石が置けない場合はスキップする処理
-    const nextDisc = disc === Disc.Dark ? Disc.Light : Disc.Dark
+    const nextDisc = this.decideNextDisc(nextBoard, disc)
 
     return new Turn(
       this._gameId,
@@ -38,6 +37,24 @@ export class Turn {
       nextBoard,
       new Date()
     )
+  }
+
+  private decideNextDisc(board: Board, previousDisc: Disc): Disc | undefined {
+    const existDarkValidMove = board.existValidMove(Disc.Dark)
+    const existLightValidMove = board.existValidMove(Disc.Light)
+
+    if (existDarkValidMove && existLightValidMove) {
+      // 両方置ける場合は、前の石と反対の石の番
+      return previousDisc === Disc.Dark ? Disc.Light : Disc.Dark
+    } else if (!existDarkValidMove && !existLightValidMove) {
+      // 両方置けない場合、次の石はない
+      return undefined
+    } else if (existDarkValidMove) {
+      // 片方しか置けない場合は、置けるほうの石の番
+      return Disc.Dark
+    } else {
+      return Disc.Light
+    }
   }
 
   get gameId() {
